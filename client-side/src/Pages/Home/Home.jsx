672fe10062446885fb9866b174d6c3b42a888330
NRS\nrsha <nrshakib007@gmail.com>
2023-08-13 14:38:56 +0600

    Added Banner to Home Page

--- client-side/src/Pages/Home/Home.jsx
+++ client-side/src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import Banner from "./Banner";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>Home</h2>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
