1c645bdec1b4f8b1bd839da29928526b7d812fd8
NRS\nrsha <nrshakib007@gmail.com>
2023-08-15 02:22:40 +0600

    Added Footer using DaisyUI

--- client-side/src/Pages/Home/Home.jsx
+++ client-side/src/Pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import Services from "./Services";
 import MakeAppointment from "./MakeAppointment";
 import Testimonials from "./Testimonials";
 import Contact from "./Contact";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       <MakeAppointment />
       <Testimonials />
       <Contact />
+      <Footer />
     </div>
   );
 };
