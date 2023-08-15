704f106982cd9a821e0fa61010b5d71fa503ebd6
NRS\nrsha <nrshakib007@gmail.com>
2023-08-16 00:08:02 +0600

    Moved Footer to shared components

--- client-side/src/Pages/Shared/Footer.jsx
+++ client-side/src/Pages/Shared/Footer.jsx
@@ -33,10 +33,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="my-10 text-center">
-        <p>
-          Copyright ©{new Date().getFullYear()} - All right reserved by ACME
-          Industries Ltd
-        </p>
+        <p>Copyright ©{new Date().getFullYear()} - All right reserved</p>
       </div>
     </footer>
   );
