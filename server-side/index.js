bcb907b9c059cab814488ce04fadb32580b14256
NRS\nrsha <nrshakib007@gmail.com>
2023-08-17 22:24:25 +0600

    connected express to mongodb 2

--- server-side/index.js
+++ server-side/index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.neciaip.mongodb.net/?retryWrites=true&w=majority`;
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
