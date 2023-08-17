22cee41017130e906f2aa47f9d11e307cedd7c20
NRS\nrsha <nrshakib007@gmail.com>
2023-08-17 18:13:40 +0600

    connected express to mongodb

--- server-side/index.js
+++ server-side/index.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.neciaip.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    const serviceCollection = client
+      .db("doctors_portal_db")
+      .collections("services");
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 //middleware
 app.use(cors());
