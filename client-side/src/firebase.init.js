72767941ce6164be809f9097b87622cb94874c8f
NRS\nrsha <nrshakib007@gmail.com>
2023-08-19 18:02:42 +0600

    Firebase auth with Google sign in

--- client-side/src/firebase.init.js
+++ client-side/src/firebase.init.js
@@ -6,15 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: import.meta.env.VITE_APP_apiKey,
+  authDomain: import.meta.env.VITE_APP_authDomain,
+  projectId: import.meta.env.VITE_APP_projectId,
+  storageBucket: import.meta.env.VITE_APP_storageBucket,
+  messagingSenderId: import.meta.env.VITE_APP_messagingSen2derId,
+  appId: import.meta.env.VITE_APP_appId,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
